ac3a351c4f21a6a75be542aed1ce137ae86a6dea
Dmytrovel <43705571+Dmytrovel@users.noreply.github.com>
2024-05-08 16:14:13 +0300

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# Threshold for CPU idle time
-threshold=5
+# Threshold for load average 
+threshold=0.05 
 
 # Monitoring period in minutes (15 minutes)
 monitoring_period=15
 
 # Initial sleep time of 2.5 hours
-sleep 900s # 9000s 
+sleep 200s 
 
 # This will create an infinite loop
 while true; do
 
-    # Check CPU idle time every minute for the next 15 minutes
+    # Check load average every minute for the next 15 minutes
     for ((i=0; i<$monitoring_period; i++)); do
-        current_idle=$(sar -u 1 1 | tail -1 | awk '{print $8}')
-        
-        if (( $(echo "$current_idle < $threshold" | bc -l) )); then
-            echo "CPU idle time is less than $threshold%, shutting down."
+        # Fetch the 1 minute load average using 'uptime'
+        load_avg=$(uptime | awk -F'load average: ' '{ print $2 }' | cut -d, -f1)
+        if (( $(echo "$load_avg > $threshold" | bc -l) )); then
+            echo "Load average is more than $threshold, shutting down."
             sudo shutdown -h now
             exit 0
         fi
         sleep 60s
     done
 
-    echo "CPU idle time was not less than $threshold% in the past 15 minutes."
+    echo "Load average was not higher than $threshold in the past 15 minutes."
 
 done
