b746220b027662dbdecf6b86babe4c6278a38937
Dmytrovel <43705571+Dmytrovel@users.noreply.github.com>
2024-05-08 23:19:50 +0300

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -3,41 +3,35 @@
 # Specify the directory you want to monitor
 dir_to_monitor="/home/$0"
 
-# How long to initially wait (in seconds)
-initial_wait_seconds= 1800 # 7200  # This is equivalent to 2 hours
-
-# How long to wait with no changes (in seconds)
-max_wait_seconds=120 # 900  # This is equivalent to 15 minutes
-
-# Threshold for load average 
-threshold=5
-
-# Monitoring period in minutes (15 minutes)
-monitoring_period=15
-
-# Initial sleep
-sleep $initial_wait_seconds
+# Threshold for CPU usage, transformed into idle time (3.1% usage -> 96.9% idle)
+threshold=96.9
 
 # This will create an infinite loop
 while true; do
     change_detected=0
 
-    # Check load average and if changes occurred in the directory
-    for ((i=0; i<$monitoring_period; i++)); do
-        load_avg=$(uptime | awk -F'load average: ' '{ print $2 }' | cut -d, -f1)
+    # Check load average as well as if changes occurred in the directory
+    for ((i=0; i<30; i++)); do
+        cpu_idle=$(sar -u 1 1 | tail -1 | awk '{print $8}')
         inotifywait -r -e modify,create,delete,move --timeout 60 $dir_to_monitor && change_detected=1
 
-        if (( $(echo "$load_avg < $threshold" | bc -l) )); then
-            echo "Load average is less than $threshold, shutting down."
-            sudo shutdown -h now
-            exit 0
-        elif [ $change_detected -eq 0 ]; then
-            echo "No changes detected within the past minute, shutting down..."
-            sudo shutdown -h now
-            exit 0
+        # If CPU usage goes above 3.1%, break the loop and reset the cycle
+        if (( $(echo "$cpu_idle < $threshold" | bc -l) )); then
+            echo "CPU usage has gone above 3.1%, resetting the cycle."
+            break
         fi
+
         sleep 60s
     done
 
-    echo "Neither load average dropped below the threshold nor absence of changes in the past 15 minutes detected."
+    # Shut down the system if CPU usage never exceeded 3.1% AND no changes occurred in the directory
+    if (( $(echo "$cpu_idle >= $threshold" | bc -l) )) && [ $change_detected -eq 0 ]; then
+        echo "CPU usage remained below 3.1% and no file changes detected for half an hour, shutting down..."
+        sudo shutdown -h now
+
+        # Exit the script since the machine is going to be shut down
+        exit 0
+    fi
+
+    echo "No shutdown triggered, starting a new cycle..."
 done
