1b46d7fcbd07e7ce11b675fe732331479715ff27
Dmytrovel <43705571+Dmytrovel@users.noreply.github.com>
2024-05-09 12:06:11 +0300

    Update inotifywait.sh

--- inotifywait.sh
+++ inotifywait.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Вказати шлях до каталогу, який ви хочете відстежувати
+# Specify the directory path that you want to monitor
 dir_to_monitor="/home/$0"
 
-# Шляхи до файлів журналу
+# Logfile paths
 log_file="/var/log/directory.log"
 changes_log_file="/var/log/directory_changes.log"
 
-# Функція для реєстрації повідомлень з відміткою часу
+# Function to log timestamped messages
 log_message() {
     local timestamp=$(date +"%Y-%m-%d %T")
     echo "[$timestamp] $1" >> "$log_file"
@@ -15,16 +15,16 @@ log_message() {
 
 while true; do
     change_detected=0
-
-    # Очікування змін протягом однієї години
+    
+    # Monitor for changes for one hour
     if inotifywait -r -e modify,create,delete,move --timeout 3600 $dir_to_monitor; then
         change_detected=1
-        echo "[$(date +"%Y-%m-%d %T")] Виявлені зміни в $dir_to_monitor" >> "$changes_log_file"
+        echo "[$(date +"%Y-%m-%d %T")] Changes detected in $dir_to_monitor" >> "$changes_log_file"
     fi
 
-    # Якщо зміни не були виявлені протягом однієї години, виконати вимкнення системи
+    # If no changes were detected within one hour, log and shut down
     if [ $change_detected -eq 0 ]; then
-        log_message "Протягом останньої години змін не виявлено, вимкнення системи..."
+        log_message "No file changes detected for last hour, shutting down..."
         sudo shutdown -h now
         exit 0
     fi
