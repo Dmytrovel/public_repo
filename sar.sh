2959c6c31c6a06f4bf0d043ea61be94932738765
Dmytrovel <43705571+Dmytrovel@users.noreply.github.com>
2024-05-10 10:28:08 +0300

    Update sar.sh

--- sar.sh
+++ sar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Threshold for CPU usage (Average CPU usage should be less than this value for 3 hours)
-threshold=5
+# threshold=5
+threshold=$3
 
 # Logfile path
 log_file="/var/log/cpu_idel.log"
@@ -17,12 +18,14 @@ log_message() {
 sar_threshold=$(echo "100 - $threshold" | bc)
 
 # Initial sleep of 2.5 hours
-sleep 9000s
+# sleep 9000s
+sleep $1
 
 # Run indefinitely
 while true; do
     # Get the average CPU idle time over the past 30 minutes
-    cpu_avg_idle=$(sar -u 30 1 | grep "Average" | awk '{print $NF}')
+    # cpu_avg_idle=$(sar -u 30 1 | grep "Average" | awk '{print $NF}')
+    cpu_avg_idle=$(sar -u $2 1 | grep "Average" | awk '{print $NF}')
 
     if (( $(echo "$cpu_avg_idle > $sar_threshold" | bc -l) )); then
         log_message "Average CPU usage has remained below 5% for the past 30 minutes, shutting down..."
